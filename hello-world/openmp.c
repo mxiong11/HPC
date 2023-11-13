f3828140539b68c5ce4f34f34a9fea3325e1803b
mxiong11 <gjwv42@login2.ham8.dur.ac.uk>
2023-11-13 20:55:51 +0000

    Sync all changes to the repository

---
+++ hello-world/openmp.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <omp.h>
+
+int main(void)
+{
+  int nthread = omp_get_max_threads();
+  int thread;
+#pragma omp parallel private(thread) shared(nthread)
+  {
+    thread = omp_get_thread_num();
+    if(thread == nthread -1)
+    {
+       printf("Hello, World! I am thread %d of %d\n", thread, nthread);
+    }
+  }
+  return 0;
+}
